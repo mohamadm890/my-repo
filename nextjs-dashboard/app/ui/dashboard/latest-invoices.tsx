44422ad92a5bafd5e80239a8bf34b8c98d0f9c16
mohamadm890 <94972216+mohamadm890@users.noreply.github.com>
2023-10-31 04:43:08 -0700

    Update latest-invoices.tsx

--- nextjs-dashboard/app/ui/dashboard/latest-invoices.tsx
+++ nextjs-dashboard/app/ui/dashboard/latest-invoices.tsx
@@ -1,7 +1,6 @@
 import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
 import { LatestInvoice } from '@/app/lib/definitions';
 export default async function LatestInvoices({
   latestInvoices,
@@ -10,7 +9,7 @@ export default async function LatestInvoices({
 }) {
   return (
     <div className="flex w-full flex-col md:col-span-4 lg:col-span-4">
-      <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+      <h2 className="mb-4 text-xl md:text-2xl">
         Latest Invoices
       </h2>
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
